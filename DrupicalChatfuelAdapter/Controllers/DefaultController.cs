6ff0ad19ccc4810dbdc09ea3ed534756b6338e80
FF <f+git@fida.biz>
2016-09-20 19:24:11 +0200

    fix search

--- DrupicalChatfuelAdapter/Controllers/DefaultController.cs
+++ DrupicalChatfuelAdapter/Controllers/DefaultController.cs
@@ -24,13 +24,22 @@ namespace DrupicalChatfuelAdapter.Controllers
         }
     }
 
+    public static class StringSearchExt
+    {
+        public static bool Search (this string heystack, string needle )
+        {
+            if (string.IsNullOrWhiteSpace(heystack) || string.IsNullOrWhiteSpace(needle)) return false;
+            return heystack.IndexOf(needle, StringComparison.InvariantCultureIgnoreCase) != -1;
+        }
+    }
+
     public class DefaultController : Controller
     {
 
         private ICacheService _cache = new InMemoryCache();
 
         // GET: Default
-        public ActionResult Index(int? limit= 10, IEnumerable<string> type = null, IEnumerable<string> city = null, IEnumerable<string> country = null, string format = null)
+        public ActionResult Index(int? limit = 9, IEnumerable<string> type = null, IEnumerable<string> city = null, IEnumerable<string> country = null, string format = null, string search = null)
         {
             var url = "https://www.drupical.com/app";
             var defaultLogo = "https://www.dropbox.com/s/l75ltlq9vy8i7iq/drupical-cover.png?dl=1";
@@ -47,7 +56,25 @@ namespace DrupicalChatfuelAdapter.Controllers
             if (type != null && type.Any()) drupical = drupical.Where(d => type.Contains(d.type, comparer)).ToList();
             if (city != null && city.Any()) drupical = drupical.Where(d => city.Contains(d.city, comparer)).ToList();
             if (country != null && country.Any()) drupical = drupical.Where(d => country.Contains(d.country, comparer)).ToList();
-            if (limit.HasValue && limit.Value > -1) drupical = drupical.Take(limit.Value).ToList();
+            if (search != null)
+            {
+                drupical = drupical.Where(d 
+                    => d.title.Search(search)
+                    || d.country.Search(search)
+                    || d.city.Search(search)
+                    || d.state.Search(search)
+                    || d.venue.Search(search)
+                    || d.thoroughfare.Search(search)
+                    || d.postal_code.Search(search)
+                ).ToList();
+            }
+
+            var appendDrupicalElement = 0;
+            if (limit.HasValue && limit.Value > -1)
+            {
+                if (drupical.Count > limit.Value) appendDrupicalElement = drupical.Count - limit.Value;
+                drupical = drupical.Take(limit.Value).ToList();
+            }
 
             var chatfuel = new List<Chatfuel>();
             var chatfuelElements = new List<Element>();
@@ -68,7 +95,7 @@ namespace DrupicalChatfuelAdapter.Controllers
                     subtitle = string.Join(", ", subtitle),
                     buttons = new[]
                     {
-                        new Button()
+                        new Button
                         {
                             type = "web_url",
                             url = d.link,
@@ -77,6 +104,20 @@ namespace DrupicalChatfuelAdapter.Controllers
                     }
                 });
             }
+
+            if(appendDrupicalElement > 0) chatfuelElements.Add(new Element
+            {
+                title = "There is a lot happening",
+                subtitle = $"{appendDrupicalElement} more over at Drupical.com",
+                image_url = "https://www.dropbox.com/s/lqknjods7yn8rmi/drupical-link.png?dl=1",
+                buttons = new []
+                {
+                    new Button
+                    {
+                        title = "Show them all", url = "https://www.drupical.com/", type = "web_url"
+                    }, 
+                }
+            });
 
             chatfuel.Add(new Chatfuel
             {
